235d03b29cc4f24a0ad78d35c25de5e7caa517bb
Vlad Lakeev <chilli.willi.930@gmail.com>
2024-08-10 09:36:46 +0300

    volunteer model added

---
+++ backend/src/PetHomeFinder.Domain/SocialNetwork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHomeFinder.Domain
+{
+    public class SocialNetwork
+    {
+        public string Name { get; private set; }
+        public string Link { get; private set; }
+    }
+}
---
+++ backend/src/PetHomeFinder.Domain/Volunteer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHomeFinder.Domain
+{
+    public class Volunteer
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Surname { get; private set; }
+        public string Description { get; private set; }
+        public int Experience { get; private set; }
+        public int PetHomeFoundCount { get; private set; }
+        public int PetSearchForHomeCount { get; private set; }
+        public int PetTreatmentCount { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public List<SocialNetwork> SocialNetworks { get; private set; }
+        public List<Credential> Credentials { get; private set; }
+        public List<Pet> PetsOwning { get; private set; }
+    }
+}
