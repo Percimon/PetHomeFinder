6498c499e084291dce371628eb313c935cfe9537
Vlad Lakeev <chilli.willi.930@gmail.com>
2024-10-11 11:50:12 +0300

    update controllers, enveloper and response extensions added

--- backend/src/PetHomeFinder.API/Controllers/VolunteersController.cs
+++ backend/src/PetHomeFinder.API/Controllers/VolunteersController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using PetHomeFinder.API.Extensions;
 using PetHomeFinder.Application.Volunteers.Create;
+using PetHomeFinder.Application.Volunteers.UpdateCredentials;
+using PetHomeFinder.Application.Volunteers.UpdateMainInfo;
+using PetHomeFinder.Application.Volunteers.UpdateSocialNetworks;
 
 namespace PetHomeFinder.API.Controllers
 {
@@ -20,9 +24,56 @@ namespace PetHomeFinder.API.Controllers
         }
 
         [HttpPut("{id:guid}/main-info")]
-        public async Task<ActionResult> UpdateMainInfo()
+        public async Task<ActionResult> UpdateMainInfo(
+        [FromRoute] Guid id,
+        [FromServices] UpdateMainInfoHandler handler,
+        [FromBody] UpdateMainInfoRequest request,
+        CancellationToken cancellationToken = default)
         {
+            var command = new UpdateMainInfoRequest(
+                id,
+                request.FullName,
+                request.Description,
+                request.Experience,
+                request.PhoneNumber);
 
+            var result = await handler.Handle(command, cancellationToken);
+            if (result.IsFailure)
+                result.Error.ToResponse();
+
+            return Ok(result.Value);
+        }
+
+        [HttpPut("{id:guid}/credentials")]
+        public async Task<ActionResult> UpdateCredentials(
+            [FromRoute] Guid id,
+            [FromServices] UpdateCredentialsHandler handler,
+            [FromBody] UpdateCredentialsRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var command = new UpdateCredentialsRequest(id, request.CredentialList);
+
+            var result = await handler.Handle(command, cancellationToken);
+            if (result.IsFailure)
+                result.Error.ToResponse();
+
+            return Ok(result.Value);
+        }
+
+        [HttpPut("{id:guid}/social-networks")]
+        public async Task<ActionResult> UpdateSocialNetworks(
+            [FromRoute] Guid id,
+            [FromServices] UpdateSocialNetworksHandler handler,
+            [FromBody] UpdateSocialNetworksRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            var command = new UpdateSocialNetworksRequest(id, request.SocialNetworkList);
+
+            var result = await handler.Handle(command, cancellationToken);
+            if (result.IsFailure)
+                result.Error.ToResponse();
+
+            return Ok(result.Value);
         }
 
     }
